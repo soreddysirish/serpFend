b55db82695fce5c1a65a4eca7f305cda40a057be
soreddysirish <ssireeshreddy@gmail.com>
2019-07-04 10:32:10 +0530

    individual category call added

--- src/components/category_page.jsx
+++ src/components/category_page.jsx
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
+import queryString from 'query-string'
+import axios from "axios";
+import Promise from "promise"
+import { host } from "./helper";
 class CategoryPage extends Component {
-    render(){
-        return(
+    constructor(props) {
+        super(props)
+        this.state = {
+            category_name: ''
+        }
+    }
+    componentDidMount() {
+        let _self = this
+        let parsed = queryString.parse(this.props.location.search);
+        _self.setState({
+            category_name: parsed["name"]
+        })
+        setTimeout(function(){
+            if(_self.state.category_name){
+                return new Promise(function(resolve){
+                    axios.get(host()+"/category/"+_self.state.category_name).then(function(json){
+                        debugger
+                    }).catch(function(err){
+                    })
+                })
+            }
+        },1000)
+    }
+    render() {
+        return (
             <div>
                 <h2>Category  page</h2>
             </div>

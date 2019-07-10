ef6009051ae43da4b062d0bfe128f699259af1fd
soreddysirish <ssireeshreddy@gmail.com>
2019-07-10 10:29:57 +0530

    helper js changed

--- src/components/helper.jsx
+++ src/components/helper.jsx
@@ -1,11 +1,9 @@
 export const host = function () {
-    let host = window.location.hostname;
-    if (host === "13.251.49.54") {
-      return "http://13.251.49.54:82";
-    }else if(host === "13.250.127.183"){
-      return "http://13.250.127.183:82";
-    } else {
-      return "http://localhost:3000";
-    }
-    return host;
-  };
+  let host = window.location.hostname;
+  if (host === "13.250.127.183") {
+    return "http://13.250.127.183:5001";
+  } else {
+    return "http://localhost:3000";
+  }
+  return host;
+};

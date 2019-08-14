f38d782146f3e5f5ee97fa54d7ca75cbbc6e80d3
soreddysirish <ssireeshreddy@gmail.com>
2019-08-14 18:10:56 +0530

    pem file path changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       ref: "origin/master",
       repo: "git@github.com:soreddysirish/serpFend.git",
       path: "/var/www/serp_frontend",
-      key: "/Users/giridharr/Documents/projects/cleartrip/key_files/dynamic-flights-high-config.pem",
+      key: "/Users/sireeshreddy/Documents/cleartrip/pempfiles/imp/cleartrip_pem/dynamic-flights-high-config.pem",
       ssh_options: ["ForwardAgent=yes"],
       "post-deploy": "npm install --production && npm run build"
     }
